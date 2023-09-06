98032be0f7d8de29f2ff6fa1e12dec7dcc95721c
belvesta <143174841+belvesta@users.noreply.github.com>
2023-09-06 23:30:57 +0300

    Sem1Task1_2

--- Sem1Task1/Program.cs
+++ Sem1Task1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine(num1 == Math.Pow(num2, 2) ? "Да" : "Нет");

6b5d09c45fdec26c237a45d0a75d52a8b147eda7
belvesta <143174841+belvesta@users.noreply.github.com>
2023-09-11 18:11:01 +0300

    Sem1Task6_2

--- Sem1Task6/Program.cs
+++ Sem1Task6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая на входе принимает число и выдаёт,
+// является ли число чётным (делится ли оно на 2 без остатка).
+System.Console.WriteLine("Введите число: ");
+   string number=Console.ReadLine();
+   int number1=int.Parse(number);
+       if (number1%2==1)
+       {
+        Console.WriteLine("Число "+number1+" является нечётным.");
+       }
+       else
+       {
+         Console.WriteLine("Число "+number1+" является чётным.");
+       }

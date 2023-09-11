528dfbf2fe0e9c3e9d5eb6268aed8aeeffa14f5e
belvesta <143174841+belvesta@users.noreply.github.com>
2023-09-11 18:25:08 +0300

    Sem1Task8_2

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая на вход принимает число N, а на выходе
+// показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=n; i++)
+{
+   if (i%2==0)
+   System.Console.Write(i+", "); 
+}       

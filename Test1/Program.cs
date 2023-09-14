a8e025132c95670ff8b9e605cc764638904ce80d
belvesta <143174841+belvesta@users.noreply.github.com>
2023-09-14 23:06:32 +0300

    Sem2Task15_1

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -10,10 +10,14 @@ using System;
 
 public class Answer
 {
-    static bool IsPalindrome(int number){
+
+
+    static bool IsPalindrome(int number)
+    {
       // Введите свое решение ниже
-      { for (var i = 0; i < str.Length / 2; ++i) { if (str[i] != str[str.Length - 1 - i]) { return false; } } return true; }
-      IsPalindrome(number.ToString());
+      
+
+      
     }
   
   // Не удаляйте и не меняйте метод Main! 

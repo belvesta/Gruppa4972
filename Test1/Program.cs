fd65d6fbb09cd05e97581a24e804ae029d16a89a
belvesta <143174841+belvesta@users.noreply.github.com>
2023-09-18 08:46:22 +0300

    все папки до Sem4Task30

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -12,14 +12,31 @@ public class Answer
 {
 
 
-    static bool IsPalindrome(int number)
-    {
-      // Введите свое решение ниже
-      
+    static bool IsPalindrome(int number) {
+   
+              // Введите свое решение ниже
+      if(number>99999||number<10000)
+      {
+       Console.WriteLine("Число не пятизначное.");
+       return false;
+      }
+      else
+      {
+        int d1=number/10000;
+        int d2=(number/10000)%10;
+        int d3=(number/10)%10;
+        int d4=number%10;
 
-      
+        if(d1==d4&&d2==d3)
+        {
+          return true;
+        }
+        else
+        {
+            return false;
+        }
+      }
     }
-  
   // Не удаляйте и не меняйте метод Main! 
       static public void Main(string[] args) {
         int number;
